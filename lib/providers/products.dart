c7bad9087796a70874d3a366cc49bda6a0433149
Ikram khan <ikr302000@gmail.com>
2023-03-20 23:03:52 +0500

    transform fetch data

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -35,7 +35,22 @@ class Products with ChangeNotifier {
 
     try {
       final response = await http.get(url);
-      print(json.decode(response.body));
+      final extractData = json.decode(response.body) as Map<String, dynamic>;
+      final List<Product> loadedProducts = [];
+      extractData.forEach((prodId, prodData) {
+        loadedProducts.add(
+          Product(
+            id: prodId,
+            title: prodData['title'],
+            description: prodData['description'],
+            price: prodData['price'],
+            imageUrl: prodData['imageUrl'],
+            isFavorite: prodData['isFavorite'],
+          ),
+        );
+      });
+      _items = loadedProducts;
+      notifyListeners();
     } catch (error) {
       rethrow;
     }
@@ -88,36 +103,36 @@ class Products with ChangeNotifier {
   }
 }
 
-final List<Product> _items = [
-  Product(
-    id: 'p1',
-    title: 'Red Shirt',
-    description: 'A red shirt - it is pretty red!',
-    price: 29.99,
-    imageUrl:
-        'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
-  ),
-  Product(
-    id: 'p2',
-    title: 'Trousers',
-    description: 'A nice pair of trousers.',
-    price: 59.99,
-    imageUrl:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
-  ),
-  Product(
-    id: 'p3',
-    title: 'Yellow Scarf',
-    description: 'Warm and cozy - exactly what you need for the winter.',
-    price: 19.99,
-    imageUrl: 'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
-  ),
-  Product(
-    id: 'p4',
-    title: 'A Pan',
-    description: 'Prepare any meal you want.',
-    price: 49.99,
-    imageUrl:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
-  ),
+List<Product> _items = [
+//   Product(
+//     id: 'p1',
+//     title: 'Red Shirt',
+//     description: 'A red shirt - it is pretty red!',
+//     price: 29.99,
+//     imageUrl:
+//         'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+//   ),
+//   Product(
+//     id: 'p2',
+//     title: 'Trousers',
+//     description: 'A nice pair of trousers.',
+//     price: 59.99,
+//     imageUrl:
+//         'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+//   ),
+//   Product(
+//     id: 'p3',
+//     title: 'Yellow Scarf',
+//     description: 'Warm and cozy - exactly what you need for the winter.',
+//     price: 19.99,
+//     imageUrl: 'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+//   ),
+//   Product(
+//     id: 'p4',
+//     title: 'A Pan',
+//     description: 'Prepare any meal you want.',
+//     price: 49.99,
+//     imageUrl:
+//         'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+//   ),
 ];
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -36,10 +36,18 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   }
 
   var _isInit = true;
+  var _isLoading = false;
   @override
   void didChangeDependencies() {
     if (_isInit) {
-      Provider.of<Products>(context).fetchAndSetProducts();
+      setState(() {
+        _isLoading = true;
+      });
+      Provider.of<Products>(context).fetchAndSetProducts().then((value) {
+        setState(() {
+          _isLoading = false;
+        });
+      });
     }
     _isInit = false;
     super.didChangeDependencies();
@@ -106,6 +114,10 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
           ],
         ),
         drawer: AppDrawer(),
-        body: ProductsGrid(_showOnlyFavorites));
+        body: _isLoading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : ProductsGrid(_showOnlyFavorites));
   }
 }
