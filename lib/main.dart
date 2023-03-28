d363873ab2cd4d053f9850cb7833a61ec709e19d
Ikram khan <ikr302000@gmail.com>
2023-03-29 00:15:24 +0500

    adding the auth screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import "package:flutter/material.dart";
 import 'package:shop_app/providers/cart.dart';
 import 'package:shop_app/providers/orders.dart';
 import 'package:shop_app/providers/products.dart';
+import 'package:shop_app/screens/auth_screen.dart';
 import 'package:shop_app/screens/cart_screen.dart';
 import 'package:shop_app/screens/edit_product_screen.dart';
 import 'package:shop_app/screens/orders_screen.dart';
@@ -41,7 +42,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
         title: 'IKi Shop',
-        home: ProductsOverviewScreen(),
+        home: AuthScreen(),
         routes: {
           ProductDetailScreen.routName: (context) => ProductDetailScreen(),
           CartScreen.routName: (context) => CartScreen(),
