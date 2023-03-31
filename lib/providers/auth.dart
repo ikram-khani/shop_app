5d9c7493ee13e9698507584a415288e6b642b8ab
Ikram khan <ikr302000@gmail.com>
2023-03-31 22:49:45 +0500

    using lean code

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -9,9 +9,10 @@ class Auth with ChangeNotifier {
   DateTime? _expiryDate;
   String? _userId;
 
-  Future<void> signup(String email, String password) async {
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
     final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=$firebaseProjectKey');
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=$firebaseProjectKey');
 
     final response = await http.post(
       url,
@@ -21,21 +22,14 @@ class Auth with ChangeNotifier {
         'returnSecureToken': true,
       }),
     );
-
     print(json.decode(response.body));
   }
 
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
   Future<void> login(String email, String password) async {
-    final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=$firebaseProjectKey');
-
-    final response = await http.post(url,
-        body: json.encode({
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        }));
-
-    print(json.decode(response.body));
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
