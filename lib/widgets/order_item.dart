e90b1c495ff0366780e6de834c4b5e41b1ccf1eb
Ikram khan <ikr302000@gmail.com>
2023-05-20 13:46:45 +0500

    animating order boxes

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -16,27 +16,33 @@ class _OrderItemState extends State<OrderItem> {
   var _expanded = false;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(10),
-      child: Column(children: [
-        ListTile(
-          title: Text('\$${widget.order.amount.toStringAsFixed(2)}'),
-          subtitle: Text(
-            DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      height:
+          _expanded ? min(widget.order.products.length * 20 + 100, 200) : 95,
+      child: Card(
+        margin: const EdgeInsets.all(10),
+        child: Column(children: [
+          ListTile(
+            title: Text('\$${widget.order.amount.toStringAsFixed(2)}'),
+            subtitle: Text(
+              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
+            ),
+            trailing: IconButton(
+              onPressed: (() {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              }),
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+            ),
           ),
-          trailing: IconButton(
-            onPressed: (() {
-              setState(() {
-                _expanded = !_expanded;
-              });
-            }),
-            icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-          ),
-        ),
-        if (_expanded)
-          Container(
+          AnimatedContainer(
+            duration: const Duration(milliseconds: 300),
             padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-            height: min(widget.order.products.length * 20 + 10, 100),
+            height: _expanded
+                ? min(widget.order.products.length * 20 + 10, 100)
+                : 0,
             child: ListView(
                 children: widget.order.products
                     .map(
@@ -60,7 +66,8 @@ class _OrderItemState extends State<OrderItem> {
                     )
                     .toList()),
           ),
-      ]),
+        ]),
+      ),
     );
   }
 }
